ec56e221798814460667d927d9babcd5edaeea4f
AdvenAdam <advenadam@student.uns.com>
2021-06-11 11:24:56 +0700

    100%

--- app/Views/admin/dokumen/index.php
+++ app/Views/admin/dokumen/index.php
@@ -74,7 +74,6 @@
                                     <th>Penelitian</th>
                                     <th>Bidang</th>
                                     <th>#</th>
-                                    <th>#</th>
                                 </tr>
                             </thead>
                             <?php $i = 1; ?>
@@ -87,12 +86,6 @@
                                         <td><?= $list['kategori_dokumen']; ?></td>
                                         <td><?= $list['jenis_penelitian']; ?></td>
                                         <td><?= $list['bidang']; ?></td>
-                                        <td>
-                                            <form action="/Transaksi/Pinjam/<?= $list['id']; ?>" method="post">
-                                                <?= csrf_field(); ?>
-                                                <button class="btn btn-success">Pinjam</button>
-                                            </form>
-                                        </td>
                                         <td>
                                             <a href="Dokumen/edit/<?= $list['id']; ?>" title="Edit"><i data-feather="edit"></i> </a>
                                             <a href="Dokumen/detail/<?= $list['id']; ?>" title="Lihat"><i data-feather="eye"></i> </a>
